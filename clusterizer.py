a894bbfce92539d1d58755f66b96d72145963c89
Ubuntu <gleb@vm.m2wm3nmks1yuzbcderi33ytxsf.bx.internal.cloudapp.net>
2020-05-02 08:52:25 +0000

    added 3-class classification with divided 0-class

--- clusterizer.py
+++ clusterizer.py
@@ -5,33 +5,58 @@ from sklearn.cluster import KMeans
 
 
 class Clusterizer:
-    def __init__(self, dataset, n_clusters=2):
+    def __init__(self, dataset, n_clusters=2, divideClass0=False):
         self.n_clusters = n_clusters
         self.train_alphas = np.array([a for aa in dataset.train_data_features[2] for a in aa])
         
-        self.angle_class, self.angle_class_idxs, self.centers = self._clusterize()
+        self.angle_class, self.centers = self._clusterize()
+        
+        if divideClass0:
+            self._clusterize_class0()
+        
         self.min_max_class = [(min(c), max(c)) for c in self.angle_class]
 
 
     def _clusterize(self):
-        angle_class_idxs, angle_class = [], []
+        angle_class = []
         
         kmeans = KMeans(n_clusters=self.n_clusters)
         labels = kmeans.fit_predict(np.stack([np.cos(self.train_alphas), np.sin(self.train_alphas)], axis=1))
-        
+        centers = kmeans.cluster_centers_
+
         for i in range(self.n_clusters):
-            angle_class_idxs.append(labels == i)
             angle_class.append(self.train_alphas[labels == i])
         
         for i in range(1, self.n_clusters):
             if max(angle_class[i]) >= 3:
                 angle_class[i], angle_class[0] = angle_class[0], angle_class[i]
-                angle_class_idxs[i], angle_class_idxs[0] = angle_class_idxs[0], angle_class_idxs[i]
+                centers[[0,i]] = centers[[i,0]]
                 break
                 
-        return angle_class, angle_class_idxs, kmeans.cluster_centers_
+        return angle_class, centers
 
-    
+
+    def _clusterize_class0(self):
+        angle_class = []
+        
+        kmeans = KMeans(n_clusters=2)
+        labels = kmeans.fit_predict(np.stack([np.cos(self.angle_class[0]), np.sin(self.angle_class[0])], axis=1))
+        centers = kmeans.cluster_centers_
+
+        for i in range(2):
+            angle_class.append(self.angle_class[0][labels == i])
+            
+        self.angle_class = angle_class + self.angle_class[1:]
+        self.centers = np.concatenate([centers, self.centers[1:]], axis=0)
+        self.n_clusters += 1
+
+        for i in range(1, self.n_clusters):
+            if max(self.angle_class[i]) >= 3:
+                self.angle_class[i], self.angle_class[0] = self.angle_class[0], self.angle_class[i]
+                self.centers[[0,i]] = self.centers[[i,0]]
+                break
+
+
     def ang_to_class(self, angles):
         seq = angles.flatten()
 
